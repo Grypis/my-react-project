7b855f67e72213293d710c251983c1f382ea6352
grypis <grypisnecros@gmail.com>
2024-07-21 22:47:45 +0300

    fourth try

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,12 +1,15 @@
-// src/App.jsx
+// src/components/Product.jsx
 
-const Product = () => {
-    return (
-      <div>
-        <h2>Cookies</h2>
-              <p>Price: 999 credits</p>
-      </div>
-    );
-  };
+export const Product = () => {
+  const price = 999;
+  const imgUrl = "<https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640>";
+  return (
+    <>
+      <h2>Tacos</h2>
+			<img src={imgUrl} alt="Tacos With Lime" width="640" />
+      <p>Price: {price} credits</p>
+    </>
+  );
+};
 
   export default Product;
